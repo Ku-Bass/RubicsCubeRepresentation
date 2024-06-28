ae193c6c16d0482ed12af68f6f94ea66df579114
Jakub Černohorský <jakub.cernohorsky@mensagymnazium.cz>
2024-06-28 19:57:38 +0200

    Added other methods

--- RubicsCubeRepresentation/Cube.cs
+++ RubicsCubeRepresentation/Cube.cs
@@ -76,6 +76,356 @@ Cube.blue,Cube.blue,Cube.blue,Cube.blue,Cube.blue,Cube.blue,Cube.blue,Cube.blue,
 Cube.white, Cube.white,Cube.white,Cube.white,Cube.white,Cube.white,Cube.white,Cube.white,
 			};
 		}
+		//method for copiing a cube into another instance of the RubicsCube class
+		public Cube CloneCube()
+		{
+			int[] yellowSideN = new int[8];
+			int[] greenSideN = new int[8];
+			int[] orangeSideN = new int[8];
+			int[] redSideN = new int[8];
+			int[] blueSideN = new int[8];
+			int[] whiteSideN = new int[8];
+
+			System.Array.Copy(yellowSide, yellowSideN, yellowSide.Length);
+			System.Array.Copy(greenSide, greenSideN, greenSide.Length);
+			System.Array.Copy(orangeSide, orangeSideN, orangeSide.Length);
+			System.Array.Copy(redSide, redSideN, redSide.Length);
+			System.Array.Copy(blueSide, blueSideN, blueSide.Length);
+			System.Array.Copy(whiteSide, whiteSideN, whiteSide.Length);
+
+			return new Cube(yellowSideN, greenSideN, orangeSideN, redSideN, blueSideN, whiteSideN);
+
+		}
+
+		//method for getting the position of the cube in a single array
+		public int[] GetPositionArray()
+		{
+			int[] output = new int[48];
+			yellowSide.CopyTo(output, 0);
+
+			greenSide.CopyTo(output, 8);
+			orangeSide.CopyTo(output, 16);
+			redSide.CopyTo(output, 24);
+			blueSide.CopyTo(output, 32);
+			whiteSide.CopyTo(output, 40);
+			return output;
+		}
+
+
+		// method for scrambling the cube with a given number of random moves
+		public void Scramble(int moves)
+		{
+			for (int i = 0; i < moves; i++)
+			{
+				RandomMove();
+			}
+		}
+
+		//makes one random move
+		public void RandomMove()
+		{
+			int rng = Random.Shared.Next(1, 12);
+			switch (rng)
+			{
+				case 1:
+					{
+						R();
+						//path += "R ";
+						break;
+					}
+				case 2:
+					{
+						RPrime();
+						//path += "R' ";
+						break;
+					}
+				case 3:
+					{
+						L();
+						//path += "L ";
+						break;
+					}
+				case 4:
+					{
+						LPrime();
+						//path += "L' ";
+						break;
+					}
+				case 5:
+					{
+						U();
+						//path += "U ";
+						break;
+					}
+				case 6:
+					{
+						UPrime();
+						//path += "U' ";
+						break;
+					}
+				case 7:
+					{
+						D();
+						//path += "D ";
+						break;
+					}
+				case 8:
+					{
+						DPrime();
+						//path += "D' ";
+						break;
+					}
+				case 9:
+					{
+						F();
+						//path += "F ";
+						break;
+					}
+				case 10:
+					{
+						FPrime();
+						//path += "F' ";
+						break;
+					}
+				case 11:
+					{
+						B();
+						// path += "B ";
+						break;
+					}
+				case 12:
+					{
+						BPrime();
+						// path += "B' ";
+						break;
+					}
+			}
+		}
+
+
+		//method for making a specific move
+		/*
+		 * 1 = R
+		 * 2 = R'
+		 * 3 = L
+		 * 4 = L'
+		 * 5 = U
+		 * 6 = U'
+		 * 7 = D
+		 * 8 = D'
+		 * 9 = F
+		 * 10 = F'
+		 * 11 = B
+		 * 12 = B'
+		 * */
+		public void Move(int move)
+		{
+
+			switch (move)
+			{
+				case 1:
+					{
+						R();
+						//path += "R ";
+						break;
+					}
+				case 2:
+					{
+						RPrime();
+						//path += "R' ";
+						break;
+					}
+				case 3:
+					{
+						L();
+						//path += "L ";
+						break;
+					}
+				case 4:
+					{
+						LPrime();
+						//path += "L' ";
+						break;
+					}
+				case 5:
+					{
+						U();
+						//path += "U ";
+						break;
+					}
+				case 6:
+					{
+						UPrime();
+						//path += "U' ";
+						break;
+					}
+				case 7:
+					{
+						D();
+						//path += "D ";
+						break;
+					}
+				case 8:
+					{
+						DPrime();
+						//path += "D' ";
+						break;
+					}
+				case 9:
+					{
+						F();
+						//path += "F ";
+						break;
+					}
+				case 10:
+					{
+						FPrime();
+						//path += "F' ";
+						break;
+					}
+				case 11:
+					{
+						B();
+						// path += "B ";
+						break;
+					}
+				case 12:
+					{
+						BPrime();
+						// path += "B' ";
+						break;
+					}
+			}
+		}
+
+		//method for checking if the cube is solved (returns true if solved)
+		public bool Solved()
+		{
+
+			if (CheckIfSame(yellowSide) == false || CheckIfSame(greenSide) == false || CheckIfSame(orangeSide) == false || CheckIfSame(redSide) == false || CheckIfSame(blueSide) == false || CheckIfSame(whiteSide) == false)
+			{
+				return false;
+			}
+			else return true;
+		}
+
+		//method for checking how many squares have correct colors (solved cube = 48)
+		public int HowCloseAmI()
+		{
+			int counter = 0;
+			counter += CheckHowMuchCorrect(yellowSide);
+			counter += CheckHowMuchCorrect(greenSide);
+			counter += CheckHowMuchCorrect(orangeSide);
+			counter += CheckHowMuchCorrect(redSide);
+			counter += CheckHowMuchCorrect(blueSide);
+			counter += CheckHowMuchCorrect(whiteSide);
+			return counter;
+		}
+
+		//method for checking if a side is solved
+		bool CheckIfSame(int[] side)
+		{
+			int control = 0;
+			if (side == yellowSide)
+			{
+				control = Cube.yellow;
+			}
+			else if (side == greenSide)
+			{
+				control = Cube.green;
+			}
+			else if (side == orangeSide)
+			{
+				control = Cube.orange;
+			}
+			else if (side == redSide)
+			{
+				control = Cube.red;
+			}
+			else if (side == blueSide)
+			{
+				control = Cube.blue;
+			}
+			else if (side == whiteSide)
+			{
+				control = Cube.white;
+			}
+			foreach (int i in side)
+			{
+				if (i != control)
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+
+		//helper method for checking how many squares have correct colors on a side
+		int CheckHowMuchCorrect(int[] side)
+		{
+			int counter = 0;
+			int control = 0;
+			if (side == yellowSide)
+			{
+				control = Cube.yellow;
+			}
+			else if (side == greenSide)
+			{
+				control = Cube.green;
+			}
+			else if (side == orangeSide)
+			{
+				control = Cube.orange;
+			}
+			else if (side == redSide)
+			{
+				control = Cube.red;
+			}
+			else if (side == blueSide)
+			{
+				control = Cube.blue;
+			}
+			else if (side == whiteSide)
+			{
+				control = Cube.white;
+			}
+			foreach (int i in side)
+			{
+				if (i == control)
+				{
+					counter++;
+				}
+			}
+
+			return counter;
+		}
+
+		//helper method for printing  a side of the cube in console
+		void Print(int[] a)
+		{
+			foreach (int i in a)
+			{
+				Console.WriteLine(i);
+			}
+		}
+
+		//method for printing the whole cube in console
+		public void PrintCube()
+		{
+			Console.WriteLine("yellow");
+			Print(yellowSide);
+			Console.WriteLine("green");
+			Print(greenSide);
+			Console.WriteLine("orange");
+			Print(orangeSide);
+			Console.WriteLine("red");
+			Print(redSide);
+			Console.WriteLine("blue");
+			Print(blueSide);
+			Console.WriteLine("white");
+			Print(whiteSide);
+		}
 
 		//helper methods for making moves
 		List<int[]> ChangeThree(int[] front, int[] top, int[] back, int[] down, int a1, int a2, int a3, int b1, int b2, int b3, int c1, int c2, int c3, int d1, int d2, int d3)
